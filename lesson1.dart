237bbe37df2d862d9faed117376193bce72353c0
Ihar Skavysh <igogoblin@gmail.com>
2026-06-05 21:28:50 +0300

    feat: add day2 file

--- lesson1.dart
+++ lesson1.dart
@@ -1,9 +1,9 @@
 void main() {
-  const pi = "про дарт";
+  const name = "про дарт";
   final date = DateTime.now();
   print(date);
   print('Hello, World!');
   dynamic variable = 42;
   variable = "Hello, Dart!";
-  print(variable);
+  print(variable + " " + name);
 }
