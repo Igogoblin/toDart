94fe398b3cc784184815d21475b74613028cddbf
Ihar Skavysh <igogoblin@gmail.com>
2026-06-15 21:34:48 +0300

    feat: add 6 day file

---
+++ days/day6.dart
@@ -0,0 +1,11 @@
+void main() {
+  Set<String> passengerNames = {'Alice', 'Bob', 'Charlie', 'David', 'Eve'};
+
+  passengerNames.add('Bob'); // Duplicate, will not be added
+  if (passengerNames.contains('Alice')) {
+    print('Alice is on the passenger list.');
+  }
+
+  List<String> passengerList = passengerNames.toList(); // Convert Set to List
+  print('Passenger List: $passengerList');
+}
