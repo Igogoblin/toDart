52621fa43672e1b2d73c1cea6196f0d85a8a25b6
Ihar Skavysh <igogoblin@gmail.com>
2026-06-16 22:55:08 +0300

    refactor: change day 6 file

--- days/day6.dart
+++ days/day6.dart
@@ -8,4 +8,9 @@ void main() {
 
   List<String> passengerList = passengerNames.toList(); // Convert Set to List
   print('Passenger List: $passengerList');
+
+  List<int> numbers = [1, 2, 2, 3, 4, 4, 5];
+  Set<int> uniqueNumbers = numbers
+      .toSet(); // Convert List to Set to remove duplicates
+  print('Unique Numbers: $uniqueNumbers');
 }
