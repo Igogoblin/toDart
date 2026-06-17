744d9e4dfb925d26fc9b7b05b124b637ef4ec0c1
Ihar Skavysh <igogoblin@gmail.com>
2026-06-17 20:18:27 +0300

    feat: add set exemple in day6 file

--- days/day6.dart
+++ days/day6.dart
@@ -1,3 +1,5 @@
+import 'day4.dart';
+
 void main() {
   Set<String> passengerNames = {'Alice', 'Bob', 'Charlie', 'David', 'Eve'};
 
@@ -13,4 +15,39 @@ void main() {
   Set<int> uniqueNumbers = numbers
       .toSet(); // Convert List to Set to remove duplicates
   print('Unique Numbers: $uniqueNumbers');
+  forMap();
+  print('Unique Tags: $uniqueTags'); // Output: {dart, flutter, web, ui}
+
+  Map<String, String> userRoles = {'Alice': 'Admin', 'Bob': 'User'};
+  Map<String, String> updatedRoles = {}; // Create a copy of the original map
+  for (var entry in userRoles.entries) {
+    updatedRoles[entry.value] = entry.key; // Copy each key-value pair
+  }
+
+  print('Original Roles: $userRoles');
+  print('Updated Roles: $updatedRoles');
 }
+
+void forMap() {
+  Map<String, int> productPrices = {'Apple': 2, 'Banana': 1, 'Orange': 3};
+
+  print(productPrices['Apple']); // Output: 2
+  print(productPrices['Milk']); // Output: null
+  productPrices['Milk'] = 4; // Adding a new key-value pair
+  print(productPrices['Milk']); // Output: 4
+  productPrices.remove('Banana'); // Removing a key-value pair
+  print(productPrices); // Output: {Apple: 2, Orange: 3, Milk: 4}
+}
+
+List<String> articleTags = [
+  'dart',
+  'flutter',
+  'dart',
+  'web',
+  'flutter',
+  'ui',
+  'dart',
+];
+
+Set<String> uniqueTags = articleTags
+    .toSet(); // Convert List to Set to remove duplicates
