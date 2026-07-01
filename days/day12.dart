2812464202b5f0b7055cc40301896aa3b59dea31
Ihar Skavysh <igogoblin@gmail.com>
2026-07-01 21:56:24 +0300

    refactor: prepare day12 file

--- days/day12.dart
+++ days/day12.dart
@@ -1,1 +1,8 @@
-void main() {}
+void main() {
+  Map<String, dynamic> user = {'name': 'Илья', 'balance': 250.0};
+  if (user['balance'] is num && user['balance'] > 100) {
+    print('Баланс пользователя ${user['name']} больше 100');
+  } else {
+    print('Баланс пользователя ${user['name']} меньше или равен 100');
+  }
+}
