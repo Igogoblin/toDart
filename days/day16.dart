2889c4dcfe6682d18eb099d9dbde877ab95fa320
Ihar Skavysh <igogoblin@gmail.com>
2026-07-10 22:08:46 +0300

    feat: add solutions day16

--- days/day16.dart
+++ days/day16.dart
@@ -3,4 +3,26 @@
 Самопроверка: Попробуй изменить _balance напрямую из другого файла (или внутри DartPad, сымитировав приватность).
 */
 
-void main() {}
+void main() {
+  final account = BankAccount(1000.0);
+  print('Initial Balance: ${account.balance}'); // Выводит начальный баланс
+  account.balance = 500.0; // Устанавливаем новый баланс через сеттер
+}
+
+class BankAccount {
+  double _balance; // Приватное поле для хранения баланса
+
+  BankAccount(this._balance);
+
+  // Геттер для получения значения баланса
+  double get balance => _balance;
+
+  // Сеттер для установки значения баланса
+  set balance(double newBalance) {
+    if (newBalance < 0) {
+      print('Ошибка: Баланс не может быть отрицательным!');
+    } else {
+      _balance = newBalance;
+    }
+  }
+}
