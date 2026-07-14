42f470c110298d1cc72393495fad0c5514c4e5bc
Ihar Skavysh <igogoblin@gmail.com>
2026-07-14 20:32:53 +0300

    feat: add day20

---
+++ days/day20.dart
@@ -0,0 +1,28 @@
+/*mixin, with, расширения (extension).
+Практика: Создай миксин Logger, который имеет метод log(String msg). Подмешай его к классу AuthService. Также напиши extension для класса String, который делает первую букву заглавной (например, "dart".capitalize()).
+Самопроверка: В чем разница между extends и with?
+*/
+
+void main() {
+  final authService = AuthService();
+  authService.log("User logged in");
+  print("Dart".capitalize());
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return '${this[0].toUpperCase()}${substring(1)}';
+  }
+}
+
+mixin Logger {
+  void log(String msg) {
+    print(msg);
+  }
+}
+
+class AuthService with Logger {
+  void login() {
+    log("User logged in");
+  }
+}
