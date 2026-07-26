7ad8450af74179352160b78a9c9e8e3b2fe54a73
Ihar Skavysh <igogoblin@gmail.com>
2026-07-26 19:03:47 +0300

    feat: add loadFile

--- days/day24.dart
+++ days/day24.dart
@@ -1,1 +1,42 @@
-void main() {}
+/* Напиши симулятор загрузки файлов. У тебя есть список из 3-х «файлов» (урлов). Напиши асинхронную функцию, которая загружает их строго по очереди (через await), и другую версию, которая запускает загрузку всех файлов одновременно (Future.wait). Сравни время выполнения.
+Самопроверка: Какой способ загрузки файлов быстрее? Почему?
+*/
+
+import 'dart:async';
+import 'dart:io';
+
+Future<void> loadFile(String url) async {
+  await Future.delayed(Duration(seconds: 1));
+  print('Loaded file from $url');
+  await Future.delayed(Duration(seconds: 1));
+}
+
+Future<void> main() async {
+  final urls = [
+    'https://example.com/file1.txt',
+    'https://example.com/file2.txt',
+    'https://example.com/file3.txt',
+  ];
+  for (final url in urls) {
+    await loadFile(url);
+  }
+}
+
+Future<void> main2() async {
+  final urls = [
+    'https://example.com/file1.txt',
+    'https://example.com/file2.txt',
+    'https://example.com/file3.txt',
+  ];
+  await Future.wait(urls.map((url) => loadFile(url)));
+}
+
+void main3() async {
+  final urls = [
+    'https://example.com/file1.txt',
+    'https://example.com/file2.txt',
+    'https://example.com/file3.txt',
+  ];
+  final futures = urls.map((url) => loadFile(url));
+  await Future.wait(futures);
+}
