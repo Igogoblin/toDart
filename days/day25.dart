15496a3e344d54f29e42862ed1b5d452ffc3b62c
Ihar Skavysh <igogoblin@gmail.com>
2026-08-03 20:58:32 +0300

    feat: add stream in day25 file

--- days/day25.dart
+++ days/day25.dart
@@ -1,1 +1,16 @@
-void main() {}
+/*Stream, async*, yield, подписка на поток .listen().
+Практика: Напиши функцию-таймер countdown(int ticks) async*. Она должна каждую секунду отдавать (yield) оставшееся количество секунд. В функции main подпишись на этот поток и выводи цифры в консоль.
+Самопроверка: Какое ключевое слово заменяет return в генераторах потоков?
+*/
+void main() {
+  countdown(5).listen((value) {
+    print(value);
+  });
+}
+
+Stream<int> countdown(int ticks) async* {
+  for (int i = ticks; i > 0; i--) {
+    yield i;
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
